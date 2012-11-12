aa2101fe627b79282dd6d4c7f50ae65709bd8775
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2012-11-12 20:57:10 +0000

    Added newpage, and updated deploy script

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -4,6 +4,7 @@
 echo Starting Installation Of Web Arch Site
 mkdir -p /var/www/WebArchSite
 cp ../html/index.html /var/www/WebArchSite
+cp ../html/newpage.html /var/www/WebArchSite
 
 echo Completed.
 
